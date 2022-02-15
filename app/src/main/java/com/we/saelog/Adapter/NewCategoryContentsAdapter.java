6e8ed3dafdcb245678f7b3774be5cc72344efc1c
hyeweon <berry_na@khu.ac.kr>
2022-02-15 23:45:36 +0900

    Update : NewCategoryContentsAdapter.java

--- app/src/main/java/com/we/saelog/Adapter/NewCategoryContentsAdapter.java
+++ app/src/main/java/com/we/saelog/Adapter/NewCategoryContentsAdapter.java
@@ -1,9 +1,13 @@
 package com.we.saelog.Adapter;
 
+import android.graphics.Typeface;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -16,6 +20,7 @@ import java.util.ArrayList;
 
 public class NewCategoryContentsAdapter extends RecyclerView.Adapter<NewCategoryContentsAdapter.ViewHolder> {
     private ArrayList<Integer> contentsArrayList;
+    String[] contentTitles = new String[8];
 
     @NonNull
     @Override
@@ -38,22 +43,41 @@ public class NewCategoryContentsAdapter extends RecyclerView.Adapter<NewCategory
         return contentsArrayList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements TextWatcher {
         TextView mContentNum;
+        EditText mcontentTitle;
         Spinner mSpinner;
         ArrayAdapter contentTypeAdapter;
 
         public ViewHolder(@NonNull View itemView) {
-            //super(itemView);
+            super(itemView);
             // 항목 커스텀
             mContentNum = (TextView) itemView.findViewById(R.id.contentNum);
             mSpinner = (Spinner) itemView.findViewById(R.id.spinner);
             contentTypeAdapter = ArrayAdapter.createFromResource(itemView.getContext(), R.array.content_types, R.layout.item_content_type);
             mSpinner.setAdapter(contentTypeAdapter);
+            mcontentTitle = (EditText) itemView.findViewById(R.id.contentTitle);
+            mcontentTitle.addTextChangedListener(this);
         }
 
         public void onBind(Integer integer) {
             mContentNum.setText(integer.toString() + ". ");
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            mcontentTitle.setTypeface(Typeface.DEFAULT_BOLD);
+            contentTitles[getAdapterPosition()] = charSequence.toString();
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            mcontentTitle.setTypeface(Typeface.DEFAULT_BOLD);
         }
     }
 }
