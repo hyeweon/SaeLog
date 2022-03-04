4b9dacfbb44611696d8acf5202cf1545b91ed867
hyeweon <berry_na@khu.ac.kr>
2022-03-04 22:38:45 +0900

    Update

--- app/src/main/java/com/we/saelog/PostDetailsActivity.java
+++ app/src/main/java/com/we/saelog/PostDetailsActivity.java
@@ -1,10 +1,13 @@
 package com.we.saelog;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,11 +21,13 @@ public class PostDetailsActivity extends AppCompatActivity {
 
     PostDetailsRecyclerAdapter mRecyclerAdapter;
 
+    private ImageButton mMenu;
     private ImageButton mDownBack;
     private TextView mTitle;
 
     MyPost post;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +37,9 @@ public class PostDetailsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String title = intent.getStringExtra("title");
         post = (MyPost) intent.getSerializableExtra("post");
+
+        mMenu = findViewById(R.id.menu);
+        mMenu.setOnClickListener(clickListener);
 
         mDownBack = (ImageButton) findViewById(R.id.downBack);
         mDownBack.setOnClickListener(clickListener);
@@ -64,6 +72,28 @@ public class PostDetailsActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             switch (view.getId()){
+                case R.id.menu:
+                    PopupMenu popupMenu = new PopupMenu(getApplicationContext(), view);
+                    getMenuInflater().inflate(R.menu.post_details_items, popupMenu.getMenu());
+                    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            switch (item.getItemId()){
+                                case R.id.btnUpdate:
+
+                                    break;
+                                case R.id.btnDelete:
+
+                                    break;
+                                case R.id.btnSaveImage:
+
+                                    break;
+                            }
+                            return false;
+                        }
+                    });
+                    popupMenu.show();
+                    break;
                 case R.id.downBack:
                     onBackPressed();
                     break;
